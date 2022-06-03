d6c7f4d88362dc8b3f47ea08be8946595f5a1cd3
Finn Mattis <planto73@harkenpretty.com>
2022-06-03 17:03:07 -0400

    Added dunder name to shell.py

--- shell.py
+++ shell.py
@@ -1,7 +1,7 @@
 
 # TODO Add Zeros to Numerals
 
-# TODO Try
+# TODO Try Except
 # TODO Dictionaries
 # TODO Type function
 # TODO can't get index of index
@@ -20,86 +20,91 @@ from datetime import datetime
 
 import root
 
-#######################################
-# Date
-#######################################
 
+class Shell:
+    def __init__(self):
+        self.start()
 
-def get_time():
-    time = datetime.now().strftime("%b %d %y, %H:%M:%S")
-    time = time.replace("Jan", "Unus Martius").replace("Feb", "Duo Martius")
-    time = time.replace("Mar", "Martius").replace("Apr", "Aprilis")
-    time = time.replace("May", "Maius").replace("Jun", "Junius")
-    time = time.replace("Jul", "Quintilis").replace("Aug", "Sextilis")
-    return time
-    # Don't need to replace Sep, Oct, Nov, or Dec bc they are the same in latin
+    #######################################
+    # Date
+    #######################################
 
-#######################################
-# Help Function
-#######################################
+    def get_time(self):
+        time = datetime.now().strftime("%b %d %y, %H:%M:%S")
+        time = time.replace("Jan", "Unus Martius").replace(
+            "Feb", "Duo Martius")
+        time = time.replace("Mar", "Martius").replace("Apr", "Aprilis")
+        time = time.replace("May", "Maius").replace("Jun", "Junius")
+        time = time.replace("Jul", "Quintilis").replace("Aug", "Sextilis")
+        return time
+        # Don't need to replace Sep, Oct, Nov, or Dec bc they are the same in latin
 
+    #######################################
+    # Help Function
+    #######################################
 
-def help_menu():
-    print("\nWhat do you need help with?\n\n1: Keywords\n2: Syntax\n3: Built-In Functions\n")
-    while True:
-        help_num = input()
-        if help_num in ["1", "2", "3"]:
-            break
+    def help_menu():
+        print("\nWhat do you need help with?\n\n1: Keywords\n2: Syntax\n3: Built-In Functions\n")
+        while True:
+            help_num = input()
+            if help_num in ["1", "2", "3"]:
+                break
+            else:
+                print("\nPlease only type 1, 2, or 3\n")
+        if help_num == "1":
+            print('\nsino -> var\net -> and\naut -> or\nnon -> not\nsi -> if\nalioquinsi -> elif\nalioquin -> else\npro -> for')
+            print('ad -> to\ngradus -> step\ndum -> while\nopus -> task\nfinis -> end\nredi -> return\ncontinua -> continue\nconfringe -> break')
+        elif help_num == "2":
+            with open("grammar.txt", "r") as grammar:
+                print(grammar.read())
+        elif help_num == "3":
+            print('\nscribe(value_to_print) -> print\ninitus() -> input\npurgo() -> clear\nest_numerus(value_to_check) -> is_number\nest_filum(value_to_check) -> is_string\nest_album(value_to_check) -> is_list')
+            print('est_opus(value_to_check) -> is_function\nadde(value_to_add) -> append\nremove(value_to_remove) -> pop\nextende(list_to_extend) -> extend\nlongitudo(value_to_check) -> length\ncurre(file_to_run) -> run')
+
+    #######################################
+    # Run Script Function
+    #######################################
+
+    def run_script(fn, script):
+        result, error = root.run(fn, script)
+        if error:
+            print(error.as_string())
+        elif result:
+            result.elements = [x for x in result.elements if repr(x) != "None"]
+            if len(result.elements) == 0:
+                pass
+            elif len(result.elements) == 1:
+                print(repr(result.elements[0]))
+            else:
+                print(repr(result))
+
+    #######################################
+    # Run File from command line or take input
+    #######################################
+    def start(self):
+        if len(sys.argv) > 1:
+            try:
+                fn = sys.argv[1]
+                with open(fn, "r") as f:
+                    script = f.read()
+                self.run_script(fn, script)
+            except FileNotFoundError as e:
+                print(f"Can't open file {fn}: No such file")
+            except UnicodeDecodeError as e:
+                print(f"Can't open file {fn}: Invalid file format")
         else:
-            print("\nPlease only type 1, 2, or 3\n")
-    if help_num == "1":
-        print('\nsino -> var\net -> and\naut -> or\nnon -> not\nsi -> if\nalioquinsi -> elif\nalioquin -> else\npro -> for')
-        print('ad -> to\ngradus -> step\ndum -> while\nopus -> task\nfinis -> end\nredi -> return\ncontinua -> continue\nconfringe -> break')
-    elif help_num == "2":
-        with open("grammar.txt", "r") as grammar:
-            print(grammar.read())
-    elif help_num == "3":
-        print('\nscribe(value_to_print) -> print\ninitus() -> input\npurgo() -> clear\nest_numerus(value_to_check) -> is_number\nest_filum(value_to_check) -> is_string\nest_album(value_to_check) -> is_list')
-        print('est_opus(value_to_check) -> is_function\nadde(value_to_add) -> append\nremove(value_to_remove) -> pop\nextende(list_to_extend) -> extend\nlongitudo(value_to_check) -> length\ncurre(file_to_run) -> run')
+            time = self.get_time()
+            print(
+                f"Celeritas versio unum (defalta, {time})\nScribe 'auxilium' auxilio")
+            while True:
+                text = input('>>> ')
+                if text.strip() == "":
+                    continue
+                if text == "auxilium":
+                    self.help_menu()
+                    continue
+                self.run_script("<stdin>", text)
 
 
-#######################################
-# Run Script Function
-#######################################
-
-
-def run_script(fn, script):
-    result, error = root.run(fn, script)
-    if error:
-        print(error.as_string())
-    elif result:
-        result.elements = [x for x in result.elements if repr(x) != "None"]
-        if len(result.elements) == 0:
-            pass
-        elif len(result.elements) == 1:
-            print(repr(result.elements[0]))
-        else:
-            print(repr(result))
-
-
-#######################################
-# Run File from command line or take input
-#######################################
-
-if len(sys.argv) > 1:
-    try:
-        fn = sys.argv[1]
-        with open(fn, "r") as f:
-            script = f.read()
-        run_script(fn, script)
-    except FileNotFoundError as e:
-        print(f"Can't open file {fn}: No such file")
-    except UnicodeDecodeError as e:
-        print(f"Can't open file {fn}: Invalid file format")
-else:
-    time = get_time()
-    print(
-        f"Celeritas versio unum (defalta, {time})\nScribe 'auxilium' auxilio")
-    while True:
-        text = input('>>> ')
-        if text.strip() == "":
-            continue
-        if text == "auxilium":
-            help_menu()
-            continue
-        run_script("<stdin>", text)
+if __name__ == '__main__':
+    Shell = Shell()
