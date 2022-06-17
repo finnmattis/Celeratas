c4ff80ac762063f2882b9901b9b7ee046de8da6d
Finn Mattis <planto73@harkenpretty.com>
2022-06-17 18:03:07 -0400

    Used pytest features to make lexer tests more readable

--- lexer_test.py
+++ lexer_test.py
@@ -1,164 +1,67 @@
+import pytest
 from main.lexer import Lexer
 from helper.tokens import *
 
+# Should add testing values as well as types for stuff like strings, vars, numbers, and numerals
 
-def lexer_test(querry, expected_error, expected_length=None):
-    lexer = Lexer("<std_in>", querry)
+
+@pytest.mark.parametrize("test_input,expected", [
+    # Should be nothing
+    ("", []),
+    # Should throw error for improper tab - Tabs must either be 4 spaces or a tab unicode character
+    pytest.param(" ", [], marks=pytest.mark.xfail),
+    # Should skip empty space bc it is after first grammatical char
+    ("x ", [TT_IDENTIFIER]),
+    ("x\t", [TT_IDENTIFIER]),
+    # Should create a tab
+    ("\t", [TT_TAB]),
+    ("    ", [TT_TAB]),
+    # Check for invalid chars
+    pytest.param(".", [], marks=pytest.mark.xfail),
+    # Check for newlines
+    (";", [TT_NEWLINE]),
+    ("\n", [TT_NEWLINE]),
+    # Test comments:
+    ("#.;1", [TT_INT]),
+    # Test strings:
+    ("\"x\"", [TT_STRING]),
+    # Test identifiers:
+    ("x", [TT_IDENTIFIER]),
+    # Test ints and floats
+    ("1", [TT_INT]),
+    ("1.0", [TT_FLOAT]),
+    # Test numerals - dont test invalid numerals because that is the fault of convert roman not the lexer
+    ("IV", [TT_NUMERAL]),
+    # Test Comparison Ops
+    ("==", [TT_EE]),
+    ("!=", [TT_NE]),
+    (">", [TT_GT]),
+    ("<", [TT_LT]),
+    (">=", [TT_GTE]),
+    ("<=", [TT_LTE]),
+    # Test Binary Operators
+    ("+", [TT_PLUS]),
+    ("-", [TT_MINUS]),
+    ("*", [TT_MUL]),
+    ("/", [TT_DIV]),
+    ("^", [TT_POW]),
+    # Test Parens
+    ("(", [TT_LPAREN]),
+    (")", [TT_RPAREN]),
+    ("[", [TT_LSQUARE]),
+    ("]", [TT_RSQUARE]),
+    # Test Misc
+    ("=", [TT_EQ]),
+    (":", [TT_COLON]),
+    (",", [TT_COMMA]),
+    ("->", [TT_ARROW])
+])
+def test_lexer(test_input, expected):
+    lexer = Lexer("<std_in>", test_input)
     tokens, error = lexer.make_tokens()
 
-    if expected_error:
-        assert error != None
-    else:
-        assert error == None
+    assert error == None
+    expected.append(TT_EOF)
 
-    if expected_length:
-        assert len(tokens) == expected_length
-    return tokens
-
-
-def test_lexer_blank_space():
-
-    # Should not create any tokens besides EOF
-    tokens = lexer_test("", False, 1)
-    assert tokens[0].type == TT_EOF
-
-    # Should throw error for improper tab - Tabs must either be 4 spaces or a tab unicode character
-    tokens = lexer_test(" ", True)
-
-    # Should create only 2 tokens - Should skip empty space bc it is after first grammatical char
-    tokens = lexer_test("_ ", False, 2)
-
-    # Should create only 2 tokens - Should skip empty space bc it is after first grammatical char
-    tokens = lexer_test("x\t", False, 2)
-
-    # Should create TAB and EOF tokens
-    tokens = lexer_test("    ", False, 2)
-    assert tokens[0].type == TT_TAB
-
-    # Should create TAB and EOF tokens
-    tokens = lexer_test("\t", False, 2)
-    assert tokens[0].type == TT_TAB
-
-
-def test_lexer_invalid_chars():
-    # Should throw an error
-    tokens = lexer_test(".", True)
-
-
-def test_lexer_lines():
-
-    # Should create a new line token
-    tokens = lexer_test(";", False, 2)
-    assert tokens[0].type == TT_NEWLINE
-
-    # Should create a new line
-    tokens = lexer_test(";", False, 2)
-    assert tokens[0].type == TT_NEWLINE
-
-
-def test_lexer_comments():
-    # Should not return error
-    tokens = lexer_test("#.;1", False, 2)
-
-
-def test_lexer_strings():
-    # Should return a string token with a value of "x"
-    tokens = lexer_test("\"x\"", False, 2)
-    assert tokens[0].type == TT_STRING
-    assert tokens[0].value == "x"
-
-
-def test_lexer_identifiers():
-    # Should return an identifier with a value of "x" and no idx_to_get
-    tokens = lexer_test("x", False, 2)
-    assert tokens[0].type == TT_IDENTIFIER
-    assert tokens[0].value == "x"
-
-
-def test_lexer_numbers():
-    # Should return a int token with a value of 5
-    tokens = lexer_test("5", False, 2)
-    assert tokens[0].type == TT_INT
-    assert tokens[0].value == 5
-
-    # Should return a numeral token with a value of 4
-    tokens = lexer_test("IV", False, 2)
-    assert tokens[0].type == TT_NUMERAL
-    assert tokens[0].value == 4
-
-
-def test_lexer_comparison_ops():
-    # Should return a equals token in the middle
-    tokens = lexer_test("x == x", False, 4)
-    assert tokens[1].type == TT_EE
-
-    # Should return a not equals token in the middle
-    tokens = lexer_test("x != x", False, 4)
-    assert tokens[1].type == TT_NE
-
-    # Should return a greater than token in the middle
-    tokens = lexer_test("x > x", False, 4)
-    assert tokens[1].type == TT_GT
-
-    # Should return a less than token in the middle
-    tokens = lexer_test("x < x", False, 4)
-    assert tokens[1].type == TT_LT
-
-    # Should return a great than equals token in the middle
-    tokens = lexer_test("x >= x", False, 4)
-    assert tokens[1].type == TT_GTE
-
-    # Should return a less than equals token in the middle
-    tokens = lexer_test("x <= x", False, 4)
-    assert tokens[1].type == TT_LTE
-
-
-def test_lexer_bin_op_toks():
-
-    # Should return a plus token
-    tokens = lexer_test("+", False, 2)
-    assert tokens[0].type == TT_PLUS
-
-    # Should return a minus token
-    tokens = lexer_test("-", False, 2)
-    assert tokens[0].type == TT_MINUS
-
-    # Should return a mul token
-    tokens = lexer_test("*", False, 2)
-    assert tokens[0].type == TT_MUL
-
-    # Should return a div token
-    tokens = lexer_test("/", False, 2)
-    assert tokens[0].type == TT_DIV
-
-    # Should return a pow token
-    tokens = lexer_test("^", False, 2)
-    assert tokens[0].type == TT_POW
-
-
-def test_lexer_paren():
-    # Should return a RPAREN token
-    tokens = lexer_test("(", False, 2)
-    assert tokens[0].type == TT_LPAREN
-
-    # Should return a LPAREN token
-    tokens = lexer_test(")", False, 2)
-    assert tokens[0].type == TT_RPAREN
-
-    # Should return a LSQUARE token
-    tokens = lexer_test("[", False, 2)
-    assert tokens[0].type == TT_LSQUARE
-
-    # Should return a RSQUARE token
-    tokens = lexer_test("]", False, 2)
-    assert tokens[0].type == TT_RSQUARE
-
-
-def test_lexer_misc():
-    # Should return a colon token
-    tokens = lexer_test(":", False, 2)
-    assert tokens[0].type == TT_COLON
-
-    # Should return a comma token
-    tokens = lexer_test(",", False, 2)
-    assert tokens[0].type == TT_COMMA
+    for idx, item in enumerate(tokens):
+        assert item.type == expected[idx]
