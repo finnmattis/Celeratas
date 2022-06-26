6b43178d942b90a5feb5faef5116a368f2386b51
Finn Mattis <planto73@harkenpretty.com>
2022-06-25 20:48:43 -0400

    Started working on the interpreter tests

---
+++ interpreter_test.py
@@ -0,0 +1,22 @@
+import pytest
+from main.lexer import *
+from main.parser import *
+from main.interpreter import *
+from helper.tokens import *
+from main.root import *
+
+
+def interpreter_test_base(test_input):
+    lexer = Lexer("<stdin>", test_input)
+    tokens, _ = lexer.make_tokens()
+
+    parser = Parser(tokens)
+    ast = parser.parse()
+
+    interpreter = Interpreter()
+    context = Context('<program>')
+    context.symbol_table = global_symbol_table
+    result = interpreter.visit(ast.node, context)
+
+    assert result.error == None
+    return result.value
